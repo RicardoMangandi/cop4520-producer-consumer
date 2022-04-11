c8089f5e6116f45abf8e1935d6f91426428d2ddf
Ricardo Mangandi <ricardo.mangandi@gmail.com>
2022-04-10 23:00:58 -0400

    code format

--- task_def/do_task_1.py
+++ task_def/do_task_1.py
@@ -1,5 +1,23 @@
+from cmath import sqrt
 import time
 
-def do_task_function(param):
-    time.sleep(param)
-    True
+
+def is_prime(n):
+
+    if (n <= 1):
+        return False
+    
+    for i in range(2,n):
+        if (n % i == 0):
+            return False
+    return True
+
+
+def do_task_function(param1,param2):
+    total = 0
+    for i in range(param1,param2):
+        if is_prime(i):
+            total = total + i 
+    
+    return total
+
